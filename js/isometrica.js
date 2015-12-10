18158227ba7d7c39c0613024bf6d27d005cdfc73
Jack Herbert <jackwherbert@gmail.com>
2015-12-10 12:00:38 +0000

    Split repeated elements into reusable partials. Removed Ref numbers from Description fields. Search filters style

---
+++ js/isometrica.js
@@ -0,0 +1,57 @@
+var app = angular.module('isometrica', []);
+
+// Platform Detection (testing only)
+$( document ).ready(function() {
+	if( navigator.userAgent.match(/iPhone|iPad|iPod/i) ) {
+		var cssLink = $("<link rel='stylesheet' type='text/css' href='assets/libs/bootcards/dist/css/bootcards-ios.min.css'>");
+		$("head").append(cssLink);
+		$('body').addClass('ios');
+   	}
+	else if( navigator.userAgent.match(/Android/i) ) {
+		var cssLink = $("<link rel='stylesheet' type='text/css' href='assets/libs/bootcards/dist/css/bootcards-android.min.css'>");
+		$("head").append(cssLink);
+		$('body').addClass('android');
+	}
+	else
+	{
+		var cssLink = $("<link rel='stylesheet' type='text/css' href='assets/libs/bootcards/dist/css/bootcards-desktop.min.css'>");
+		$("head").append(cssLink);
+		$('body').addClass('desktop');
+	}
+});
+
+
+
+// DETAIL VIEW FOR MOBILE
+if ($(window).width() < 768) {
+  $( ".bootcards-list .list-group a" ).click(function() {
+
+    // Hide list view
+    $(".bootcards-list").attr('style','display:none !important');
+
+    // Show detail view
+    $(".bootcards-cards").attr('style','display:block !important');
+  });
+}
+
+
+/* Tooltips */
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+
+
+$('.btn[data-toggle="collapse"]').click(function(){
+  $(this).toggleClass("active");
+});
+
+
+
+  	/* Prevent form clicks from expanding/contracting accordion */
+  	$( ".isometrica-orgsetup-modal .form-control").click(function() {
+  		event.stopPropagation();
+  	});
+  	$( ".isometrica-orgsetup-modal .clear-field").click(function() {
+  		event.stopPropagation();
+  	});
