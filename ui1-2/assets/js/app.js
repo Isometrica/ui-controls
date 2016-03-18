7ae0dd8a10fd1cf32d02c7924a54d445f16ddc8d
Jack Herbert <jackwherbert@gmail.com>
2016-03-18 11:48:26 +0000

    Clifton updates

--- ui1-2/assets/js/app.js
+++ ui1-2/assets/js/app.js
@@ -28,31 +28,40 @@ $( document ).ready(function() {
   $('.chat-content').animate({scrollTop: $(document).height()}, 'slow');
   
   
+  // BIA Chart 
   var $biaTrendChart = $("#biaTrendChart");
-  if ($biaTrendChart.length) {
-    var data = {
-        labels: ["2014 Q2", "2014 Q3", "2014 Q4", "2015 Q1", "2015 Q2", "2015 Q3", "2015 Q4", "2016 Q1"],
-        datasets: [
-          {
-            fillColor: "#E81123",
-            strokeColor: "transparent",
-            highlightFill: "#E81123",
-            highlightStroke: "transparent",
-            data: [6500, 5900, 8000, 8100, 5600, 5500, 4000, 12300]
-          }
-        ]
-    };
-    var ctx = $biaTrendChart.get(0).getContext("2d");
-    var options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      barShowStroke : false,
-      scaleShowLabels: false,
-      scaleShowGridLines : false,
-      barValueSpacing : 10
-    };
-    new Chart(ctx).Bar(data,options);  
-  }
+  $('#bia-chart-modal').on('shown.bs.modal', function (e) {
+    if ($biaTrendChart.length) {
+      var data = {
+          labels: ["2014 Q2", "2014 Q3", "2014 Q4", "2015 Q1", "2015 Q2", "2015 Q3", "2015 Q4", "2016 Q1"],
+          datasets: [
+            {
+              fillColor: "#E81123",
+              strokeColor: "transparent",
+              highlightFill: "#E81123",
+              highlightStroke: "transparent",
+              data: [6500, 5900, 8000, 8100, 5600, 5500, 4000, 12300]
+            }
+          ]
+      };
+      var ctx = $biaTrendChart.get(0).getContext("2d");
+      var options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        barShowStroke : false,
+        //scaleShowLabels: false,
+        scaleShowGridLines : false,
+        //barValueSpacing : 10
+      };
+      new Chart(ctx).Bar(data,options);  
+    }
+  });
+  
+  $('#bia-chart-modal').on('hidden.bs.modal', function (e) {
+    if ($biaTrendChart.length) {
+        biaTrendChart.clear();
+    }
+  });
 
 });
 
